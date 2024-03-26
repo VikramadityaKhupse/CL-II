29eb584b9538c26584d56776c736e79b324ed680
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-03-26 11:09:22 +0530

    Added discord.js

--- NodeJS_Project/server.js
+++ NodeJS_Project/server.js
@@ -1,25 +1,25 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
+var nodemailer = require('nodemailer');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '2022bit052@sggs.ac.in',
+    pass: '2022@sggsIT'
   }
-}).listen(8080); 
+});
+
+var mailOptions = {
+  from: '2022bit052@sggs.ac.in',
+  to: 'vikramadityakhupse@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!',
+  html: '<h1>Welcome</h1><p>That was easy!</p>'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
