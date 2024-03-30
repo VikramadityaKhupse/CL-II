6193fc4260096b7285caa837597c344b79b2efe3
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-03-30 19:27:54 +0530

    Added initial code

--- NodeJS_Project/src/bot.js
+++ NodeJS_Project/src/bot.js
@@ -1,16 +1,18 @@
-import { config } from 'dotenv';
-config();
-const { Client, Intents } = require('discord.js');
-const client = new Client({ 
-  intents: [
-      
-      // Add more intents as needed
-  ] 
-}); // Creates new client
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 // This line must be at the very end
-client.login("MTIxMjEwOTU1ODYxNzYwODI1Mg.Gss6SI.AUMbggDXLdyAf3BXGbARogKchcuNvlFPAMzwfs"); // Signs the bot in with the token
+client.login(token); // Signs the bot in with the token
