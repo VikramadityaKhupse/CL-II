dd9c4a8adb17f1a5988b020db263fca22a25f2ca
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-04-15 21:01:40 +0530

    Littlebit changes

--- NodeJS_Project/commands/fun/say.js
+++ NodeJS_Project/commands/fun/say.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,12 +6,30 @@ module.exports = {
         .setDescription('Replies with your input!')
         .addStringOption(option =>
             option.setName('input')
-            .setDescription('The input to say back'))
+                .setDescription('The input to say back')
+                .setRequired(true))
         .addChannelOption(option =>
             option.setName('channel')
-            .setDescription('The channel to cho into')),
-    async execute(interaction){},
+                .setDescription('The channel to say it in (optional)')
+                .addChannelTypes(ChannelType.GuildText)), // Restrict to guild text channels
+    async execute(interaction) {
+        const input = interaction.options.getString('input');
+        const targetChannel = interaction.options.getChannel('channel');
 
+        // Choose the channel to send the message:
+        const channelToSendIn = targetChannel ? targetChannel : interaction.channel;
 
+        // Check if the target channel is a valid guild text channel:
+        if (targetChannel && targetChannel.type !== ChannelType.GuildText) {
+            return await interaction.reply({ content: "You can only specify a text channel for this command.", ephemeral: true });
+        }
+
+        try {
+            await channelToSendIn.send(input);
+            await interaction.reply({ content: '✅ Message sent!', ephemeral: true });
+        } catch (error) {
+            console.error('Error sending message:', error);
+            await interaction.reply({ content: 'Failed to send message. Check the console for details.', ephemeral: true });
+        }
+    },
 };
-
--- NodeJS_Project/commands/utility/ping.js
+++ NodeJS_Project/commands/utility/ping.js
@@ -8,12 +8,4 @@ module.exports = {
 		await interaction.reply('Pong!');
 	},
 };
-// const data = new SlashCommandBuilder()
-// 	.setName('say')
-// 	.setDescription('Replies with your input!')
-// 	.addStringOption(option =>
-// 		option.setName('input')
-// 			.setDescription('The input to echo back'))
-// 	.addChannelOption(option =>
-// 		option.setName('channel')
-// 			.setDescription('The channel to echo into'));
+
