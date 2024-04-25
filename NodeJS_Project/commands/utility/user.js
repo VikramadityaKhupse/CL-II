810c9a9088f97919d001af68a79f2f740d938620
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-04-25 11:50:51 +0530

    Installed Angular JS

--- NodeJS_Project/commands/utility/user.js
+++ NodeJS_Project/commands/utility/user.js
@@ -1,13 +1,19 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, Guild } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Provides information about the user.'),
+		.setDescription('Provides information about the user.')
+		.addUserOption(option =>
+			option.setName('user')
+			  .setDescription('@username')
+			  .setRequired(true)
+		  ),
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		const targetUser = interaction.options.getUser('user');
+		const guild = interaction.guild;
+		await interaction.reply(`This command was run by ${targetUser.username}, who joined on ${targetUser.joinedAt}.`);
 	},
 };
