e2aac6492eea1c1cadabfd33eeb9fee845c7500e
VikramadityaKhupse <2022bit052@sggs.ac.in>
2024-04-27 14:12:41 +0530

    Added input field etc

--- Angular_project/whats-in-my-fridge/src/app/app.component.ts
+++ Angular_project/whats-in-my-fridge/src/app/app.component.ts
@@ -3,11 +3,14 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'I am Hungry! :(';
+  firstname: String ="";
 
   constructor(){
     setTimeout(() => {
